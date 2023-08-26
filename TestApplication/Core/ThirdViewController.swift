d945d61b44b16823bc3e529d7d30648c8586682f
Shendrikov <shendricov@yandex.ru>
2023-08-26 14:28:15 +1000

    some code
    work 26.08.23 at 14:28

--- TestApplication/Core/ThirdViewController.swift
+++ TestApplication/Core/ThirdViewController.swift
@@ -11,16 +11,44 @@ class ThirdViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.view.addSubview(scrollView)
+        configureScrollView()
+    }
+    
+    private var contentSize: CGSize {
+        CGSize(width: self.view.bounds.width, height: self.view.bounds.width + 100)
     }
     
 //  MARK:  make scrollView
-    lazy private var scrollView: UIScrollView = {
+    private lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
 //        приравниваем границы самого skrollView с границам view на которой она располагается.
         scrollView.frame = self.view.bounds
-        scrollView.contentSize = CGSize(width: self.view.safeAreaInsets.right, height: self.view.bounds.height + 100)
+//        пртравниваем контент размер скролл по ширине ко вью и по выстоте более вью на сто точек.
+        scrollView.contentSize = contentSize
         return scrollView
     }()
 
+    private func contentView() -> UIView {
+        let colorBascet = [UIColor.red, .blue, .yellow, .purple]
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.widthAnchor.constraint(equalToConstant: 300),
+            contentView.heightAnchor.constraint(equalToConstant: 50)
+        ])
+        contentView.backgroundColor = colorBascet.randomElement()
+        return contentView
+    }
+    
+    
+    private func configureScrollView() {
+        let vStack = UIStackView()
+        scrollView.addSubview(vStack)
+        vStack.frame = scrollView.bounds
+        for _ in 0...30 {
+            let contentView = contentView()
+            vStack.addArrangedSubview(contentView)
+        }
+    }
 }
